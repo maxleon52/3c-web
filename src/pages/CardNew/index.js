e634749211594291a7a0e15a67554c3a63c6dacf
maxleon52 <maxleon522@gmail.com>
2020-07-16 17:48:27 -0300

    implement select

--- src/pages/CardNew/index.js
+++ src/pages/CardNew/index.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Form } from "@unform/web";
 import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 import api from "../../services/api";
 import { useAuth } from "../../hooks/AuthContext";
@@ -20,7 +21,43 @@ function CardNew() {
   const [name, setName] = useState("Nome do cartão");
   const [finalCard, setFinalCard] = useState("0000");
   const [expirationCard, setExpirationCard] = useState("");
-  const [payDay, setPayDay] = useState("25");
+  const [payDay, setPayDay] = useState([]);
+
+  const options = [
+    { value: 1, label: " 1" },
+    { value: 2, label: " 2" },
+    { value: 3, label: " 3" },
+    { value: 4, label: " 4" },
+    { value: 5, label: " 5" },
+    { value: 6, label: " 6" },
+    { value: 7, label: " 7" },
+    { value: 8, label: " 8" },
+    { value: 9, label: " 9" },
+    { value: 10, label: "10" },
+    { value: 11, label: "11" },
+    { value: 12, label: "12" },
+    { value: 13, label: "13" },
+    { value: 14, label: "14" },
+    { value: 15, label: "15" },
+    { value: 16, label: "16" },
+    { value: 17, label: "17" },
+    { value: 18, label: "18" },
+    { value: 19, label: "19" },
+    { value: 20, label: "20" },
+    { value: 21, label: "21" },
+    { value: 22, label: "22" },
+    { value: 23, label: "23" },
+    { value: 24, label: "24" },
+    { value: 25, label: "25" },
+    { value: 26, label: "26" },
+    { value: 27, label: "27" },
+    { value: 28, label: "28" },
+    { value: 29, label: "29" },
+    { value: 30, label: "30" },
+    { value: 31, label: "31" },
+  ];
+
+  console.log(payDay);
 
   async function handleSubmit(data) {
     // e.preventDefault();
@@ -29,7 +66,7 @@ function CardNew() {
 
       history.push("/cards");
     } catch (error) {
-      console.log(error);
+      console.log(error.response);
     }
   }
 
@@ -105,16 +142,17 @@ function CardNew() {
               <div className="block2">
                 <div className="block2-1">
                   <label htmlFor="pay_day">dia do pagamento</label>
-                  <Input
+                  <Select
                     name="pay_day"
-                    onChange={(e) => setPayDay(e.target.value)}
                     placeholder="ex: 25"
-                  ></Input>
+                    onChange={(e) => setPayDay(e.target.value)}
+                    options={options}
+                  />
                 </div>
 
                 <div className="block2-2">
                   <label htmlFor="best_day">melhor dia comprar</label>
-                  <Input name="best_day" placeholder="ex: 15"></Input>
+                  <Select name="best_day" placeholder="ex: 15" />
                 </div>
               </div>
 
